8d72ec682c1b93a97eb873fc638b05c929375336
Sasha Krot <sasha@dhcp-206-12-36-149.ubcvisitor.wireless.ubc.ca>
2017-12-01 16:58:31 -0800

    Dec. 1st (note: do not forget to correct paths): -got rid of 3 unnecessary fns.: DEE, DEE_RC, DSE -Because I got rid of the fns., I had to make minor changes to FEV_pass_coefficients() & FEV_RC_pass_coefficients() fns. -fixed load .rds file -implemented the following code for printing time of execution in the summary because I want to see if it make sense to print .rds or .rdata objects: ptm <- proc.time()
    file_exists_exec_time <- proc.time() - ptm
    print('Execution time (if/else) statement')
    print(file_exists_exec time)
    -implemented ‘GLOBAL_lmer_model_loaded_FLAG <- NULL’, which is a global variable. I implemented it because I went back to using the .rds file. I need the global variable so that the program does not have to re-execute the calculation of summary when I view specific sections of the summary in the main panel.
    
    need to work on:
    -I am not sure if I should stay with .rdata or .rds files. I think that .rdata is better because although it is a bit larger than .rds file (~1% bigger), it allows one to store multiple objects and the lmer_summary, which in the long-run saves a lot of execution time.
    -test code

---
+++ FEV_7_7_sidebarPanel.R
@@ -0,0 +1,40 @@
+  button_width <- 160
+
+  sidebarPanel(
+    fluidRow(
+      column(12,
+             fluidRow(column(5,div(style = "font-size: 12px;",numericInput("age","Age (year)",value = NULL,min = 0,max = 250,step = 1,width = button_width)))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",numericInput("trig","Triglycerides (mg/dl)",value = NULL,step = 0.01,width = button_width))),
+               column(5,div(style = "font-size: 12px;",numericInput("hema","Hematocrit (%)",value = NULL,min = 0,max = 100,step = 0.01,width = button_width)))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",numericInput("alb","Albumin (mg/L)",value = NULL,step = 0.01,width = button_width))),
+               column(5,div(style = "font-size: 12px;",numericInput("glob","Globulin (g/L)",value = NULL,step = 0.01,width = button_width)))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",numericInput("alk_phos","Alkaline Phosphotase",value = NULL,step = 0.01,width = button_width))),
+               column(5,div(style = "font-size: 12px;",numericInput("white_bc","White blood cells(10^9/L)",value = NULL,step = 0.01,width = button_width)))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",numericInput("qrs","QRS interval (0.01 sec)",value = NULL,step = 0.01,width = button_width))),
+               column(5,div(style = "font-size: 12px;",numericInput("alcohol","Alcohol index (ozs/wk)",value = NULL,step = 0.01,width = button_width)))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",numericInput("wine","Wine intake (glasses/wk)",value = NULL,min = 0,step = 0.01,width = button_width))),
+               column(5,div(style = "font-size: 12px;",numericInput("cocktail","Cocktail intake (drinks/wk)",value = NULL,min = 0,step = 0.01,width = button_width)))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",numericInput("height","Height (cm)",value = NULL,min = 0,step = 0.01,width = button_width))),
+               column(5,div(style = "font-size: 12px;",numericInput("cum_smoke","Cum. smoke pack-year",value = NULL,min = 0,step = 0.01,width = button_width)))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",selectInput("sex","sex",list('Not Selected','female', 'male'),selected = 'Not Selected'))),
+               column(5,div(style = "font-size: 12px;",selectInput("ba_use","Bronchodilator or aerosol",list('Not Selected','Current use', 'Former use', 'No use'),selected = 'Not Selected')))),
+             fluidRow(
+               column(5,div(style = "font-size: 12px;",selectInput("dys_exer","Dyspnea on exertion",list('Not Selected','On rigorous exercise','On moderate exercise','On slight exertion','No dyspnea on ex.'),selected = 'Not Selected'))),
+               column(5,div(style = "font-size: 12px;",selectInput("noc_s","Nocturnal symptoms",list('Not Selected','Yes', 'Maybe', 'No'),selected = 'Not Selected')))),
+
+             #action buttons
+             fluidRow(column(5, div(style = "font-size: 12px;",downloadButton("save_inputs", "Save Inputs")))),
+             fluidRow(column(10,div(style = "font-size: 12px;",fileInput("file1","Choose CSV File to Load",accept = c("text/csv","text/comma-separated-values,text/plain",".csv"),buttonLabel = "Load Inputs...")))),
+             fluidRow(column(5, div(style = "font-size: 12px;",actionButton("lmer_Submit_button", "Run Linear mixed-effects models")))),
+             fluidRow(column(5, div(style = "font-size: 12px;",actionButton("clear_inputs", "Clear Inputs")))),
+             fluidRow(column(5, div(style = "font-size: 12px;",actionButton("plot_FEV1_button", "Plot FEV1 decline")))))
+    ),
+  width=4
+)
